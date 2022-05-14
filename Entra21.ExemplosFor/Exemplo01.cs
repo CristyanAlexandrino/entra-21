5995be28e2a5bc188437eb0f25b6b56c4e806d35
AlanDominguesFernandes28 <Suporte@DESKTOP-PIOR8K9>
2022-05-13 21:26:57 -0300

    Exercicios For Feito a mostra de um relogio no primeiro exercicio

---
+++ Entra21.ExemplosFor/Exemplo01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosFor
+{
+    internal class Exemplo01
+    {
+        public void Executar()
+        {
+            for(int i = 0; i <= 23; i = i + 1)
+            {
+                for (int j = 0; j < 60; j++)
+                {
+                    for(int k = 0; k <= 59; k++)
+                    {
+                        Console.WriteLine(i + ":" + j + ":" + k);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Entra21.ExemplosFor/Program.cs
@@ -0,0 +1,6 @@
+﻿using Entra21.ExemplosFor;
+
+Console.WriteLine(@"              MENU          
+ 1 - Exemplo01
+2 - Exemplo02
+3 -Exemplo03");
