c72a45ec4c93a22b3ecd876cc7d1826521360b4a
cristyanAl <cristyanalexandrino.od@gmail.com>
2022-05-27 22:10:43 -0300

    Exercicio 02 Começo do Exercicio 2 buscar calculos de temperatura

---
+++ entra21.ExerciciosOrientaçãoObjeto/Exercicio02/Temperatura.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio02
+{
+    public class Temperatura
+    {
+        public double TemperaturaOrigem;
+        public double TemperaturaDestino;
+        public double ValorTemperatura;
+        public double GrausKelvin = 273.15;
+        public void CelsiusParaKelvin()
+        {
+           
+
+        }
+        public void CelsiusParaFahreinheit()
+        {
+
+
+        }
+        public void KelvinParaCelsius()
+        {
+
+
+        }
+        public void KelvinParaFahreinheit()
+        {
+
+
+        }
+        public void FahreinheitParaCelsius()
+        {
+
+
+        }
+        public void FahreinheitParaKelvin()
+        {
+
+
+        }
+        public void ObterTemperaturaConvertida()
+        {
+
+
+        }
+    }
+}
