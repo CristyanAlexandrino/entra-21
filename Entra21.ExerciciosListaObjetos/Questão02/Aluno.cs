212710e697361e6033df785f3146f217d35b1948
cristyan Cry <cristyanalexandrino.od@gmail.com>
2022-06-13 22:13:28 -0300

    Começo Exercicio Aluno inicio dos exercicios alunos, criada classe aluno e Enum do status aluno

---
+++ Entra21.ExerciciosListaObjetos/Questão02/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosListaObjetos.Questão02
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public int CodigoMatricula;
+        public int Idade;
+        public string MateriaFavorita;
+        public double Nota01;
+        public double Nota02;
+        public double Nota03;
+
+        public void CalcularMedia()
+        {
+
+        }
+
+        public void ObterStatus()
+        {
+
+        }
+    }
+}
