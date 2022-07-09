6b717f05d3ef9472ef5ee6494ada95f2b6a4669e
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-07-08 22:06:25 -0300

    Tabela Cidades Concluido

--- Banco de Dados/ScriptListaExercicios08.1.sql
+++ Banco de Dados/ScriptListaExercicios08.1.sql
@@ -58,3 +58,47 @@ WHERE nome LIKE 'P%';
 
 ----------------------------------------------------- Tabela Cidades --------------------------------------------------------------------------
 
+-- 1 
+SELECT estado, cidade FROM cidades;
+
+-- 2
+SELECT cidade 
+FROM cidades
+WHERE cidade LIKE 'A%';
+
+-- 3
+SELECT cidade
+FROM cidades
+WHERE cidade LIKE '%apar%';
+
+-- 4
+SELECT cidade
+FROM cidades
+WHERE cidade LIKE 'W%';
+
+-- 5
+SELECT cidade, estado 
+FROM cidades
+WHERE cidade LIKE '%tuba' 
+ORDER BY estado DESC;
+
+-- 6
+SELECT cidade 
+FROM cidades 
+WHERE LEN(cidade) > 15
+ORDER BY LEN(cidade);
+
+-- 7
+SELECT COUNT(cidade) 
+FROM cidades
+WHERE estado = 'SC'
+
+-- 8
+SELECT COUNT(cidade)
+FROM cidades
+WHERE estado = 'SP'
+
+-- 9 
+SELECT cidade
+FROM cidades
+WHERE LEN(cidade) > 10;
