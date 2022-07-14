ee8a53d340ec5d98c6c36d8ab6b8623246f49942
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-07-14 19:41:22 -0300

    Estrutura Banco de Dados e Interface exemplo de windows forms com banco de dados

---
+++ Entra21.BancoDados01.Ado.Net/Services/TipoPersonagemService.cs
@@ -0,0 +1,35 @@
+﻿using Entra21.BancoDados01.Ado.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.BancoDados01.Ado.Net.Services
+{
+    // Dois pontos seguidos de uma interface, quer dizer que a classe
+    // será obrigada a implementar os métodos(contratos) estabelecidos
+    // na interface 
+    internal class TipoPersonagemService : ITipoPersonagemService
+    {
+        public void Cadastrar(TipoPersonagem tipoPersonagem)
+        {
+            throw new NotImplementedException();
+
+            // Instanciando um objeto da classe SqlConnection,
+            // que permitirá fazer selects, isenrts, updates, deletes, etc.
+            SqlConnection conexao = new SqlConnection();
+
+            // string qie contém o caminho para o banco de dados, o
+            // que permitirá conectar ao bando de dados
+            var connectionsString = @"";
+
+            //Definir o caminho da conexão para o SqlConnection
+            conexao.ConnectionString = connectionsString;
+
+            // Abrir a conexão com o banco de dados
+             conexao.Open();
+        }
+    }
+}
